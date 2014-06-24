16ba473d7f91442754e348097ee545c06415ac2d
marcsantiago <marcanthonysanti@gmail.com>
2014-06-23 21:00:38 -0400

    Update pythagorean_theorem_solver.py

--- pythagorean_theorem_solver.py
+++ pythagorean_theorem_solver.py
@@ -8,13 +8,13 @@ b = raw_input('Enter b\t')
 c = raw_input('Enter c\t')
 
 if a.lower() == 'x':
-     solution = math.sqrt((int(c)**2) - (int(b)**2))
-	   print('A: ' + str(solution))
+    solution = math.sqrt((int(c)**2) - (int(b)**2))
+	print('A: ' + str(solution))
 	
 if b.lower() == 'x':
-	   solution = math.sqrt((int(c)**2) - (int(a)**2))
+	 solution = math.sqrt((int(c)**2) - (int(a)**2))
      print('B: ' +  str(solution))
 
 if c.lower() == 'x':
-	   solution = math.sqrt((int(a)**2) + (int(b)**2))
-	   print('C: ' + str(solution))
+	 solution = math.sqrt((int(a)**2) + (int(b)**2))
+	 print('C: ' + str(solution))
