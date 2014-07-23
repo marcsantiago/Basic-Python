2c5e7705235a6c7c88b09a816226326cea4414a9
marcsantiago <marcanthonysanti@gmail.com>
2014-07-23 12:28:09 -0400

    Create polybius.py

---
+++ polybius.py
@@ -0,0 +1,12 @@
+#! /urs/bin/env python
+polybuis_dict = {'A' : 11,'B' : 12,'C' : 13,'D' : 14,'E' : 15, 'F' : 21,'G' : 22,'H' : 23,'I' : 24,'J' : 24,'K' : 25,'L' : 31,'M' : 32,'N' : 33,'O' : 34, 'P' : 35,'Q' : 41,'R' : 42,'S' : 43,'T' : 44,'U' : 45,'V' : 51,'W' : 52,'X' : 53,'Y' : 54,'Z' : 55}
+cin = raw_input()
+l = []
+for i in xrange(int(cin)):
+	string =  raw_input()
+	for letter in string:
+		for key, value in polybuis_dict.items():
+			if letter == key:
+				l.append(value)
+	print str(l).replace(',', '')[1:-1]
+	l = []
